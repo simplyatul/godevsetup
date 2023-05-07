658e9628367137e73347e843ea64d87354b22596
Atul Thosar <atulthosar@gmail.com>
2023-05-07 14:41:59 +0530

    returning hostname in rest response

--- src/go/main.go
+++ src/go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"net/http"
 )
 
 func getHostName() string {
@@ -11,10 +13,19 @@ func getHostName() string {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	fmt.Printf("Hostname is : %s\n", hn)
 	return hn
 }
 
+func handleRoot(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hostname: %s\n", getHostName())
+}
+
+func handleRequests() {
+    http.HandleFunc("/", handleRoot)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 func main() {
-	hn := getHostName()
-	fmt.Printf("Hostname is : %s\n", hn)
+	handleRequests()
 }
